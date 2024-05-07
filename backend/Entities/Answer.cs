8b5c8f6a2491e59588a4a07b068c84de31a104fa
haidang <nonome2810@gmail.com>
2024-05-07 09:09:22 +0700

    create entity

---
+++ backend/Entities/Answer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Entities
+{
+    public class Answer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("total")]
+        public string? Total { get; set; }
+
+        [Column("exam_id")]
+        public int? ExamId { get; set; }
+
+        [Column("user_id")]
+        public int? UserId { get; set; }
+
+        [Column("attempt_id")]
+        public int? AttemptId { get; set; }
+
+        public virtual Exam? Exam { get; set; }
+        public virtual User? User { get; set; }
+        public virtual Attemp? Attemp { get; set; }
+    }
+}
