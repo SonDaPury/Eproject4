e02ff92ee45768440221602c39f0b60f936b0637
Tran Thanh Dat <dat.tt.2041>
2024-06-06 09:03:37 +0700

    Fix Exam

--- backend/Service/Interface/IExamService.cs
+++ backend/Service/Interface/IExamService.cs
@@ -4,19 +4,21 @@ using backend.Entities;
 
 namespace backend.Service.Interface
 {
-    public interface IExamService 
+    public interface IExamService
     {
         Task<int> EndExam(
             //List<UserAnswer> userAnswers,
             int examId, int userId);
         Task<dynamic> GetDetailsExam(int examId);
         //Task StartExam(int examId);
-        Task<Exam> CreateAsync(Exam exam,int index);
-        Task<(List<Exam>,int)> GetAllAsync(Pagination pagination);
+        Task<Exam> CreateAsync(Exam exam, int index);
+        Task<(List<Exam>, int)> GetAllAsync(Pagination pagination);
         Task<Exam?> GetByIdAsync(int id);
         Task<Exam?> UpdateAsync(int id, Exam updatedItem);
         Task<bool> DeleteAsync(int id);
         Task<List<Exam>> GetAllAsync();
-        Task<Question> CreateQuestionsForExamAsync(QuestionForExamDto questionDto, int examId);
+        Task<object> CreateQuestionsForExamAsync(QuestionForExamDto questionDto);
+        Task<object> UpdateQuestionandOption(UpdateQuestionDto updateQuestionDto);
+        Task<object> ConnectExamWithQuestion(ConnectExamWithQuestion questions);
     }
 }
